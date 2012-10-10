92cbd89bcd50f67fe791a6ae01e3d9c485b8a6e0
zengdj916 <zengdj916@yahoo.cn>
2012-10-10 20:24:44 +0800

    修改java文件

--- src/org/nlpr/ir/util/HelloGitHub.java
+++ src/org/nlpr/ir/util/HelloGitHub.java
@@ -8,6 +8,7 @@ public class HelloGitHub {
     public static void main(String[] args) {
         // TODO Auto-generated method stub
         System.out.println("wqe");
+        String a = new String();
     }
 
 }
